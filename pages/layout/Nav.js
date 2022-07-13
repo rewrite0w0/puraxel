67d32eb4273669b7d02ce5a9c4b4176aab95b2ca
oh taejun <rewrite0w0@gmail.com>
2022-07-13 17:58:13 +0900

    add: initial page

---
+++ pages/layout/Nav.js
@@ -0,0 +1,16 @@
+import NavIconButton from "../component/Nav/NavIconButton";
+
+export default function Nav() {
+  return (
+    <>
+      <h1>Nav</h1>
+      <span>로고</span>
+      <span>Me</span>
+      <span>Hair</span>
+      <span>FX-5000</span>
+      <span>빈~ 공간</span>
+      <span>SHOP</span>
+      <span>문의</span>
+    </>
+  );
+}
