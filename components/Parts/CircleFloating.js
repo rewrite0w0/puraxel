e8cef20af3a883d3fe61e54d29a4f8c3106a6052
oh taejun <rewrite0w0@gmail.com>
2022-07-21 09:23:27 +0900

    add: floating circle

--- components/Parts/CircleFloating.js
+++ components/Parts/CircleFloating.js
@@ -2,35 +2,48 @@ import { motion } from "framer-motion";
 
 export default function CircleFloating(props) {
   return (
-    <div
-      style={{
-        height: !props.height ? 1600 : props.height + 400,
-        width: !props.width ? 1600 : props.width + 400,
-        padding: 10,
-        margin: 10,
-      }}
-    >
+    // <div
+    //   style={{
+    //     height: !props.height ? 1600 : props.height + 400,
+    //     width: !props.width ? 1600 : props.width + 400,
+    //     padding: 10,
+    //     margin: 10,
+    //   }}
+    // >
+    <div style={{ width: 2000, height: 2000, padding: 10, margin: 10 }}>
       <motion.div
-        animate={{ y: -100, x: 0 }}
+        // className={style.box}
+        animate={{ y: 500, x: 0 }}
         transition={{
           duration: 2,
           delay: 0.25,
-          repeat: 1000,
+          repeat: Infinity,
           repeatType: "reverse",
         }}
         style={{
-          height: !props.height ? 120 : props.height,
-          width: !props.width ? 120 : props.width,
+          width: 200,
+          height: 200,
           borderRadius: "50%",
-          backgroundColor: !props.color ? "blue" : props.color,
-          background: "green",
-          // backgroundColor: `linearGradient(
-          //   141.94deg, rgba(255, 228, 249, 0.09) 17.48%, rgba(255, 228, 243, 0.3) 84.05%
-          // )`,
-          // boxShadow: "0px 16px 40px rgba(189,46,81,0.03)",
-          filter: `blur(${!props.blur ? "6" : props.blur}px)`,
+          background: "rgba(245, 40, 145, 0.06)",
+          filter: `blur(6px)`,
         }}
+        // style={{
+        //   height: !props.height ? 120 : props.height,
+        //   width: !props.width ? 120 : props.width,
+        //   borderRadius: "50%",
+        //   backgroundColor: !props.color ? "blue" : props.color,
+        //   background: "green",
+        // backgroundColor: `linearGradient(
+        //   141.94deg, rgba(255, 228, 249, 0.09) 17.48%, rgba(255, 228, 243, 0.3) 84.05%
+        // )`,
+        // boxShadow: "0px 16px 40px rgba(189,46,81,0.03)",
+        // filter: `blur(${!props.blur ? "6" : props.blur}px)`,
+        // }}
       />
     </div>
   );
 }
+
+// backdrop-filter
+
+// 투명투명
