3a739d68d73a24bf288feaaac95820deb584e78c
oh taejun <rewrite0w0@gmail.com>
2022-07-21 17:13:49 +0900

    add: playground

---
+++ pages/playground/amupage.js
@@ -0,0 +1,60 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// import LinkButton from "../../component/Body/LinkButton";
+import LinkButton from "components/Body/LinkButton";
+
+import LinkPriceButton from "components/Body/LinkPriceButton";
+
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+import slideStlye from "styles/Slide.module.css";
+import { Mousewheel, Pagination, Keyboard, Navigation, Autoplay } from "swiper";
+// import CircleFloating from "../component/CircleFloating";
+// import LinkPriceButton from "../component/Body/LinkPriceButton";
+
+export default function Amupage() {
+  return (
+    <>
+      <header>
+        <h1>HEY</h1>
+
+        {/* <LinkButton buttonName="쇼핑몰" /> */}
+        {/* <LinkPriceButton /> */}
+      </header>
+      <Swiper
+        slidesPerView={1}
+        // centeredSlides={true}
+        mousewheel={{ sensitivity: 0.12 }}
+        pagination={{
+          clickable: true,
+          // type: "progressbar",
+        }}
+        // navigation={true}
+        keyboard={{ enabled: true, pageUpDown: true }}
+        autoplay={{ delay: 100 }}
+        // style={{ height: 1000, width: 2000 }}
+        className={slideStlye.slide}
+        speed={1000}
+        rewind={true}
+        // loop={true}
+        modules={[Mousewheel, Pagination, Keyboard, Navigation, Autoplay]}
+      >
+        <SwiperSlide style={{ background: "greenyellow" }}>
+          <header>헤더헤더</header>
+          {/* <CircleFloating height={160} width={200} blur={50} /> */}
+          <main>메인메인</main>
+          <footer>푸터푸터</footer>
+        </SwiperSlide>
+        <SwiperSlide style={{ background: "pink" }}>Slide 2</SwiperSlide>
+        <SwiperSlide style={{ background: "green" }}>Slide 3</SwiperSlide>
+        <SwiperSlide style={{ background: "hotpink" }}>Slide 4</SwiperSlide>
+        <SwiperSlide style={{ background: "skyblue" }}>Slide 5</SwiperSlide>
+        <SwiperSlide style={{ background: "yellow" }}>Slide 6</SwiperSlide>
+        <SwiperSlide style={{ background: "gray" }}>Slide 7</SwiperSlide>
+        <SwiperSlide style={{ background: "#f0a0f9" }}>Slide 8</SwiperSlide>
+        <SwiperSlide style={{ background: "tomato" }}>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
---
+++ pages/playground/index.js
@@ -0,0 +1,11 @@
+import Layout from "components/layout/Layout";
+
+export default function dashboard() {
+  return (
+    <>
+      <Layout>
+        <h1>Hello dashboard</h1>
+      </Layout>
+    </>
+  );
+}
