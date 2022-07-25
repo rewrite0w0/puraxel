b51416e89bb98072fb30810e6226814643567bf5
oh taejun <rewrite0w0@gmail.com>
2022-07-25 17:28:37 +0900

    mod: layout

--- components/layout/MeLayout.js
+++ components/layout/MeLayout.js
@@ -1,7 +1,6 @@
 import { Box } from "@mui/material";
 import MeFooterRight from "components/Footer//MeFooterRight";
 import MeFooterLeft from "components/Footer//MeFooterLeft";
-import MeFooter from "components/Footer/MeFooter";
 import style from "styles/LayoutMeFooter.module.css";
 
 export default function MeLayout({ children }) {
