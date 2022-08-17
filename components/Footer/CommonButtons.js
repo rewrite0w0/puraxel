c919863d53c519e44e12b3217cec7c9463a0a23c
oh taejun <rewrite0w0@gmail.com>
2022-08-17 16:52:04 +0900

    mod: me footer buttons

--- components/Footer/CommonButtons.js
+++ components/Footer/CommonButtons.js
@@ -4,36 +4,39 @@ export default function CommonButtons(props) {
   return (
     <section
       {...props}
+      style={{ ...props.style }}
       className={_.buttonContainer}
       onClick={() => {
         window.open(props.link ? props.link : "/", "_blank");
       }}
     >
       <div className={_.buttonInnerContainer}>
-        <span className={_.topLeft}>{props.topLeft}</span>
-        <span>
-          <span className={_.topRight}>
-            {props.topRightOriginalPrice ? props.topRightOriginalPrice : ""}
+        <div className={_.lineContainer}>
+          <span className={_.topLeft}>{props.topLeft}</span>
+          <span>
+            <span className={_.topRight}>
+              {props.topRightOriginalPrice ? props.topRightOriginalPrice : ""}
+            </span>
+            <span className={_.topRightDiscount}>
+              {props.topRightDiscountPrice}
+            </span>
           </span>
-          <span className={_.topRightDiscount}>
-            {props.topRightDiscountPrice}
-          </span>
-        </span>
-      </div>
+        </div>
 
-      <div className={_.buttonInnerContainer}>
-        <span className={_.botLeft}>
-          {props.bottomLeft ? props.bottomLeft : ""}
-        </span>
-        <span>
-          <span className={_.botRightz}>
-            {props.bottomRightz ? props.bottomRightz : ""}
+        <div className={_.lineContainer}>
+          <span className={_.botLeft}>
+            {props.bottomLeft ? props.bottomLeft : ""}
           </span>
-          &nbsp;
-          <span className={_.botRightx}>
-            {props.bottomRightx ? props.bottomRightx : ""}
+          <span>
+            <span className={_.botRightz}>
+              {props.bottomRightz ? props.bottomRightz : ""}
+            </span>
+            &nbsp;
+            <span className={_.botRightx}>
+              {props.bottomRightx ? props.bottomRightx : ""}
+            </span>
           </span>
-        </span>
+        </div>
       </div>
     </section>
   );
--- components/layout/MeLayout.js
+++ components/layout/MeLayout.js
@@ -21,7 +21,7 @@ export default function MeLayout({ children }) {
           <div style={{ display: "flex" }}>
             <CommonPlainButtons
               title={layoutFooterKr.ShoppingMallLink}
-              link="https://qwant.com"
+              link="https://lmdt.cafe24.com"
               icon={
                 <ShopIcon
                   sx={{
@@ -33,7 +33,7 @@ export default function MeLayout({ children }) {
                 />
               }
               style={{
-                width: "128px",
+                width: "100px",
                 marginRight: "20px",
                 fontSize: "18px",
                 fontWeight: "600",
@@ -42,6 +42,7 @@ export default function MeLayout({ children }) {
                 cursor: "pointer",
               }}
             />
+
             <CommonButtons
               topLeft="퓨라셀 ME"
               topRightDiscountPrice="1,890,000원"
@@ -50,29 +51,22 @@ export default function MeLayout({ children }) {
               style={{
                 width: "277px",
                 marginRight: "20px",
-                fontSize: "18px",
-                fontWeight: "600",
-                letterSpacing: "-0.01em",
-                color: "#555768",
               }}
-              link="https://qwant.com"
+              link="https://lmdt.cafe24.com"
             />
 
             <CommonButtons
               topLeft="퓨라셀 ME + 퓨라더마앰플"
-              topRightOriginalPrice="2,433,000원 => "
-              topRightDiscountPrice="1,890,000원"
-              bottomLeft="23%"
+              // topRightOriginalPrice="2,433,000원 -> "
+              topRightOriginalPrice={<OriginalPrice />}
+              topRightDiscountPrice="1,977,600원"
+              bottomLeft="23% off"
               bottomRightz="12개월"
               bottomRightx="월 157,500원"
               style={{
-                width: "425px",
-                fontSize: "18px",
-                fontWeight: "600",
-                letterSpacing: "-0.01em",
-                color: "#555768",
+                width: "417px",
               }}
-              link="https://qwant.com"
+              link="https://lmdt.cafe24.com"
             />
           </div>
           {/* <CommonManualFractionButtons
@@ -84,3 +78,23 @@ export default function MeLayout({ children }) {
     </main>
   );
 }
+
+const OriginalPrice = () => {
+  return (
+    <span>
+      <span style={{ marginRight: "4px" }}>2,433,000원</span>
+      <svg
+        width="11"
+        height="8"
+        viewBox="0 0 11 8"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10.3536 4.35355C10.5488 4.15829 10.5488 3.84171 10.3536 3.64645L7.17157 0.464467C6.97631 0.269205 6.65973 0.269205 6.46447 0.464467C6.2692 0.659729 6.2692 0.976311 6.46447 1.17157L9.29289 4L6.46447 6.82843C6.2692 7.02369 6.2692 7.34027 6.46447 7.53553C6.65973 7.7308 6.97631 7.7308 7.17157 7.53553L10.3536 4.35355ZM-4.37114e-08 4.5L10 4.5L10 3.5L4.37114e-08 3.5L-4.37114e-08 4.5Z"
+          fill="#C7CAD3"
+        />
+      </svg>
+    </span>
+  );
+};
