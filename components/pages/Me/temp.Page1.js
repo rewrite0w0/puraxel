8ecf60576471d14bea5a1d8ff23616801391873f
oh taejun <rewrite0w0@gmail.com>
2022-08-17 20:28:41 +0900

    mod: me page1 container

--- components/pages/Me/temp.Page1.js
+++ components/pages/Me/temp.Page1.js
@@ -5,20 +5,17 @@ export default function Page0() {
     <main className={style.container}>
       <section className={style.leftContainer}>
         <span className={style.subTitle}>에스테틱 레이저의 시작</span>
-        <br />
         <span className={style.title}>PURAXEL ME</span>
-        <br style={{ marginBottom: "60px" }} />
+
         <span className={style.mainExplain}>레이저 기술로</span>
-        <br />
-        <span className={style.mainExplain}>
+        <span className={style.mainExplainSub} style={{ marginBottom: "24px" }}>
           <span className={style.mainExplainBold}>에스테틱에 혁신</span>을
           가져오다
         </span>
-        <br style={{ marginBottom: "24px" }} />
+
         <span className={style.subExplain}>
           병원, 의료기관에서만 볼 수 있던 프락셔널 레이저 기기,
         </span>
-        <br />
         <span className={style.subExplain}>
           이제는 집에서도 편리하게 사용하실 수 있습니다.
         </span>
