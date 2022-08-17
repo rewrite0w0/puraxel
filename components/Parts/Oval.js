8177c3593840da1a43ff25cf516cc6575037644e
oh taejun <rewrite0w0@gmail.com>
2022-08-17 20:38:21 +0900

    mod: oval style v2

--- components/Parts/Oval.js
+++ components/Parts/Oval.js
@@ -5,7 +5,7 @@ const Oval = (props) => {
   const transitionOval = useSpring({
     from: { y: props.startY, x: props.startX },
     to: { y: props.EndY, x: props.EndX },
-    config: { config: config.stiff, duration: 20000 },
+    config: { config: config.stiff, duration: 10000 },
     ...props,
   });
 
@@ -14,8 +14,6 @@ const Oval = (props) => {
       style={{
         position: "absolute",
         zIndex: 1,
-        left: 0,
-        top: 0,
         ...transitionOval,
       }}
       className={style.oval}
