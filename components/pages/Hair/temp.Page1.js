3253c414bd4148c0ed7064ef7f72518803391a2c
oh taejun <rewrite0w0@gmail.com>
2022-08-23 10:42:26 +0900

    mod: hair page1

--- components/pages/Hair/temp.Page1.js
+++ components/pages/Hair/temp.Page1.js
@@ -4,73 +4,58 @@ export default function Page0() {
   return (
     <main className={_.container}>
       <section className={_.leftContainer}>
-        <span className={_.subTitle}>에스테틱의 시작</span>
-        <span className={_.title}>PURAXEL HAIR</span>
+        <div className={_.subTitle}>에스테틱의 시작</div>
+        <div className={_.title}>PURAXEL HAIR</div>
 
-        <span
+        <div
           style={{
             fontSize: "40px",
             letterSpacing: "-0.02em",
             color: "#4D5058",
             fontWeight: "300",
+            lineHeight: "128%",
           }}
         >
           레이저 기술로
-        </span>
-        <span style={{ marginBottom: "24px" }}>
-          <span
-            style={{
-              fontSize: "48px",
-              letterSpacing: "-0.02em",
-              color: "#55576F",
-              fontWeight: "600",
-            }}
-          >
-            에스테틱에 혁신
-          </span>
-          <span
-            style={{
-              fontSize: "40px",
-              letterSpacing: "-0.02em",
-              color: "#4D5058",
-              fontWeight: "300",
-            }}
-          >
-            을 가져오다
-          </span>
-        </span>
-        <span
+        </div>
+
+        <div
+          style={{
+            fontSize: "48px",
+            letterSpacing: "-0.02em",
+            color: "#55576F",
+            fontWeight: "600",
+            marginBottom: "24px",
+            lineHeight: "128%",
+          }}
+        >
+          에스테틱에 혁신을 가져오다
+        </div>
+
+        <div
           style={{
             fontSize: "24px",
             letterSpacing: "-0.02em",
             color: "#4D5058",
             fontWeight: "300",
+            lineHeight: "140%",
           }}
         >
           퓨라셀 PURAXEL은
-        </span>
-        <span
+        </div>
+        <div
           style={{
             fontSize: "24px",
             letterSpacing: "-0.02em",
             color: "#4D5058",
             fontWeight: "300",
+            lineHeight: "140%",
           }}
         >
-          에스테틱에서 만날 수 있는
-        </span>
-        <span
-          style={{
-            fontSize: "24px",
-            letterSpacing: "-0.02em",
-            color: "#4D5058",
-            fontWeight: "300",
-          }}
-        >
-          고품격 미용 레이저입니다.
-        </span>
+          에스테틱에서 만날 수 있는 고품격 미용 레이저입니다.
+        </div>
       </section>
-      <section style={{ display: "flex" }}>
+      <section className={_.rightContainer}>
         <div className={_.rightImage}></div>
       </section>
     </main>
