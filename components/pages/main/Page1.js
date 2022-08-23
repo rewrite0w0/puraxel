a058967a61777c7605b6b4b1e748e62b8f0904cf
oh taejun <rewrite0w0@gmail.com>
2022-08-23 13:28:09 +0900

    mod: main page sentence

--- components/pages/main/Page1.js
+++ components/pages/main/Page1.js
@@ -4,61 +4,29 @@ export default function Page1() {
   return (
     <section className={_.container}>
       <article className={_.explainContainer}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            // height: "50%",
-            // justifyContent: "space-evenly",
-            justifyContent: "center",
-            // background: "blue",
-            padding: "20px",
-            height: "100%",
-          }}
-        >
+        <div className={_.explain_inner_container}>
           <div
             style={{
               display: "flex",
               flexDirection: "column",
               marginTop: "48px",
             }}
+            className={_.explain_wrap_container}
           >
-            <div style={{ marginBottom: "20px" }}>
-              <span className={_.icon}>고품격 홈 레이저 퓨라셀</span>
+            <div className={_.explain_top}>
+              <span style={{ fontWeight: "500" }}>홀</span>로 빛나다
             </div>
-            <div>
-              <span className={_.iconTitle}>
-                홈 에스테틱
-                <br />
-                레이저의 시작
-              </span>
-            </div>
-          </div>
+            <div className={_.explain_mid}>
+              <div>레이저의 미세한 홀이</div>
 
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              marginTop: "96px",
-            }}
-          >
-            <div style={{ marginBottom: "24px" }}>
-              <span className={_.mainExp}>
-                프락셔널 레이저 모듈로 생성되는
-                <br />
-                5000개의 마이크로 홀.
-              </span>
+              <div>당신의 피부를 지켜줍니다.</div>
             </div>
-            <br />
-            <div>
-              <span className={_.subExp}>
-                레이저, 시작부터 프로페셔널.
-                <br />
-                홈 레이저 뷰티 케어의 시작.
-                <br />
-                퓨라셀 ME.
-                <br />
-              </span>
+
+            <div className={_.explain_bot_title}>소형 미용 기기의 혁신</div>
+
+            <div className={_.explain_bot_sub}>
+              <div>레이저 전문 기업이 만들어</div>
+              <div>더욱 안전한 소형 프락셔널 레이저</div>
             </div>
           </div>
         </div>
