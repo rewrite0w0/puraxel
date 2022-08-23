710a73350d502b6a3fa42b064f97a95f30fe75c0
oh taejun <rewrite0w0@gmail.com>
2022-08-23 13:32:49 +0900

    mod: me page1 sentence

--- components/pages/Me/temp.Page1.js
+++ components/pages/Me/temp.Page1.js
@@ -4,24 +4,27 @@ export default function Page0() {
   return (
     <main className={style.container}>
       <section className={style.leftContainer}>
-        <span className={style.subTitle}>에스테틱 레이저의 시작</span>
-        <span className={style.title}>PURAXEL ME</span>
+        <div className={style.subTitle}>에스테틱 레이저의 시작</div>
+        <div className={style.title}>PURAXEL ME</div>
 
-        <span className={style.mainExplain}>레이저 기술로</span>
-        <span className={style.mainExplainSub} style={{ marginBottom: "24px" }}>
-          에스테틱에 혁신을 가져오다
-        </span>
+        <div className={style.mainExplain}>전문 피부과에서만 사용하던</div>
+        <div className={style.mainExplainSub}>
+          프락셔널 레이저의 문턱을 낮췄습니다.
+        </div>
 
-        <span className={style.subExplain}>
+        <div className={style.explain_sub}>
+          하루 10분, 이제 집에서 관리하세요.
+        </div>
+        {/* <div className={style.subExplain}>
           병원, 의료기관에서만 볼 수 있던 프락셔널 레이저 기기,
-        </span>
-        <span className={style.subExplain}>
+        </div>
+        <div className={style.subExplain}>
           이제는 집에서도 편리하게 사용하실 수 있습니다.
-        </span>
+        </div> */}
       </section>
-      <span className={style.rightContainer}>
-        <span className={style.meImage} />
-      </span>
+      <div className={style.rightContainer}>
+        <div className={style.meImage} />
+      </div>
     </main>
   );
 }
