56483c177cc616550f2fdc847817bb416a59746b
oh taejun <rewrite0w0@gmail.com>
2022-08-25 16:56:57 +0900

    mod: hair page1

--- components/pages/Hair/Page1.js
+++ components/pages/Hair/Page1.js
@@ -9,28 +9,8 @@ export default function Page0() {
         </div>
         <div className={_.title}>PURAXEL HAIR</div>
 
-        <div
-          style={{
-            fontSize: "48px",
-            letterSpacing: "-0.02em",
-            color: "#55576F",
-            fontWeight: "600",
-            lineHeight: "128%",
-          }}
-        >
-          머리카락이 돋아나는 마법을
-        </div>
-        <div
-          style={{
-            fontSize: "40px",
-            letterSpacing: "-0.02em",
-            color: "#4D5058",
-            fontWeight: "300",
-            lineHeight: "128%",
-          }}
-        >
-          경험하실 수 있습니다.
-        </div>
+        <div className={_.explain}>머리카락이 돋아나는 마법을</div>
+        <div className={_.subExplain}>경험하실 수 있습니다.</div>
       </section>
       <section className={_.rightContainer}>
         <div className={_.rightImage}></div>
