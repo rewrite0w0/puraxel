e5f391fe06258cf8b3b5b3a6dbfd19905e2e846b
oh taejun <rewrite0w0@gmail.com>
2022-09-06 13:47:56 +0900

    add: tech page 1440, 1024

--- components/pages/puraTech/Page1.js
+++ components/pages/puraTech/Page1.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import _ from "./page1.module.css";
 
 const ExplainContainer = (props) => {
@@ -127,6 +128,145 @@ const PuraxelTitle = () => {
   );
 };
 
+const Step1024 = (props) => {
+  return (
+    <div className={_.step1024__container}>
+      <div className={`${_.step1024__image__containers}`}>{props.image}</div>
+      <div>{props.explain}</div>
+    </div>
+  );
+};
+
+const Step1024Image01 = () => {
+  return (
+    <div className={`${_.step1024__image__container}`}>
+      <div className={`${_.step1024__image__common} ${_.step1024__image01}`} />
+    </div>
+  );
+};
+
+const Step1024Explain01 = () => {
+  return (
+    <div
+      className={`${_.step1024__explain__common} ${_.step1024__explain__container}`}
+    >
+      <div
+        className={`${_.step1024__explain01} ${_.step1024__explain__common__para}`}
+      >
+        <div className={_.step1024__explain__common__para__title}>
+          레이저 조사
+        </div>
+        <div>
+          퓨라셀 레이저
+          <br /> 침투 깊이
+          <br />
+          <span className={_.step1024__explain__common__para__bold}>
+            0.06~0.2mm
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Step1024Image02 = () => {
+  return (
+    <div className={`${_.step1024__image__container}`}>
+      <div className={`${_.step1024__image__common} ${_.step1024__image02}`} />
+    </div>
+  );
+};
+
+const Step1024Explain02 = () => {
+  return (
+    <div
+      className={`${_.step1024__explain__common} ${_.step1024__explain__container}`}
+    >
+      <div
+        className={`${_.step1024__explain02} ${_.step1024__explain__common__para}`}
+      >
+        <div className={_.step1024__explain__common__para__title}>
+          채널 생성
+        </div>
+        <div>
+          1회 약{" "}
+          <span className={_.step1024__explain__common__para__bold}>
+            100개의 미세한 마이크로
+            <br />홀
+          </span>{" "}
+          생성
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Step1024Image03 = () => {
+  return (
+    <div className={`${_.step1024__image__container}`}>
+      <div className={`${_.step1024__image__common} ${_.step1024__image03}`} />
+    </div>
+  );
+};
+
+const Step1024Explain03 = () => {
+  return (
+    <div
+      className={`${_.step1024__explain__common} ${_.step1024__explain__container}`}
+    >
+      <div
+        className={`${_.step1024__explain03} ${_.step1024__explain__common__para}`}
+      >
+        <div className={_.step1024__explain__common__para__title}>
+          유효 성분 전달
+        </div>
+        <div>
+          마이크로홀을
+          <br /> 통해{" "}
+          <span className={_.step1024__explain__common__para__bold}>
+            유효 성분을 피부 속으로 전달
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Step1024Image04 = () => {
+  return (
+    <div className={`${_.step1024__image__container}`}>
+      <div className={`${_.step1024__image__common} ${_.step1024__image04}`} />
+    </div>
+  );
+};
+
+const Step1024Explain04 = () => {
+  return (
+    <div
+      className={`${_.step1024__explain__common} ${_.step1024__explain__container}`}
+    >
+      <div
+        className={`${_.step1024__explain04} ${_.step1024__explain__common__para}`}
+      >
+        <div className={_.step1024__explain__common__para__title}>
+          피부속 앰플 흡수
+        </div>
+        <div>
+          화장품의&nbsp;
+          <span className={_.step1024__explain__common__para__bold}>
+            흡수율 향상
+          </span>
+          시켜{" "}
+          <span className={_.step1024__explain__common__para__bold}>
+            효능 <br />
+            극대화
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const PuraxelExplain = () => {
   return (
     <article className={`${_.inner__container} ${_.inner__container__right}`}>
@@ -145,14 +285,13 @@ const PuraxelExplain = () => {
         >
           <span className={_.puraxel__explain__para}>
             레이저 빔이 Array Lens를 통과하며, 피부에 약 100개의 미세한
-            구멍(마이크로 홀)을
-            <br />
+            구멍(마이크로 홀)을&nbsp;
+            <br className={_.puraxel__expliain__br__1024} />
             생성합니다. 이 때, 생성되는 마이크로 홀은 피부의 약 0.2mm 이하의
             표피 조직에 위치하게
-            <br /> 되며, 이 미세한 홀들을 통해
-            <span className={_.puraxel__explain__para__bold}>
-              기능성 화장품 등의 영양 물질 흡수율을 높일 수 있습니다.
-            </span>
+            <br className={_.puraxel__expliain__br__1024} /> 되며, 이 미세한
+            홀들을 통해 &nbsp;
+            <span>기능성 화장품 등의 영양 물질 흡수율을 높일 수 있습니다.</span>
           </span>
         </div>
 
@@ -161,11 +300,9 @@ const PuraxelExplain = () => {
         >
           <span className={_.puraxel__explain__para}>
             퓨라셀은 2940nm 파장 대의&nbsp;
-            <span className={_.puraxel__explain__para__bold}>
-              Er:Yag 레이저를 사용
-            </span>
-            합니다. Er:Yag는 표피의 수분에 대부분
-            <br />
+            <span>Er:Yag 레이저를 사용</span>
+            합니다. Er:Yag는 표피의 수분에 대부분 &nbsp;
+            <br className={_.puraxel__expliain__br__1024} />
             흡수되는 성질을 갖고 있으며, 열손상이 적어 천공 깊이의 조절이 가능한
             장점이 있습니다.
           </span>
@@ -177,6 +314,33 @@ const PuraxelExplain = () => {
         <Step2 />
         <Step3 />
         <Step4 />
+      </div>
+
+      <div className={_.puraxel__explain__container__1024}>
+        <div className={_.puraxel__explain__container__inner__1024}>
+          <div className={_.puraxel__explain__container__sub}>
+            레이저&갈바닉
+          </div>
+          <div className={_.puraxel__explain__container__title}>사용 단계</div>
+          <div className={_.puraxel__explain__container__step__container__1024}>
+            <Step1024
+              image={<Step1024Image01 />}
+              explain={<Step1024Explain01 />}
+            />
+            <Step1024
+              image={<Step1024Image02 />}
+              explain={<Step1024Explain02 />}
+            />
+            <Step1024
+              image={<Step1024Image03 />}
+              explain={<Step1024Explain03 />}
+            />
+            <Step1024
+              image={<Step1024Image04 />}
+              explain={<Step1024Explain04 />}
+            />
+          </div>
+        </div>
       </div>
     </article>
   );
