dc53813970612025a79ed1f5d0fb8ff247b87c22
oh taejun <rewrite0w0@gmail.com>
2022-09-26 14:43:32 +0900

    add: 영상에  modal 추가함

--- components/pages/Hair/Page4.js
+++ components/pages/Hair/Page4.js
@@ -1,6 +1,54 @@
+import { Modal } from "@mui/material";
+import { useState } from "react";
 import _ from "./page4.module.css";
 
+const YoutubeThumnail = () => {
+  return (
+    <iframe
+      className={_.youtube__thumnail}
+      width="100%"
+      height="100%"
+      src={`https://www.youtube-nocookie.com/embed/sNwV5GaGODY?controls=1&rel=0&disablekb=1&autoplay=0&loop=0&mute=0&modestbranding=1`}
+      title="How to use PURAXEL"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  );
+};
+
 export default function Page4() {
+  const [youtubeModalOpen, setYoutubeModalOpen] = useState(false);
+
+  const handleYoutubeModalOpen = () => {
+    return setYoutubeModalOpen(true);
+  };
+
+  const handleYoutubeModalClose = () => {
+    return setYoutubeModalOpen(false);
+  };
+
+  const YoutubeModal = () => {
+    return (
+      <Modal
+        onClickCapture={(e) => console.log(e)}
+        onClose={handleYoutubeModalClose}
+        open={youtubeModalOpen}
+      >
+        <iframe
+          className={_.youtube__modal}
+          width="70%"
+          height="70%"
+          src={`https://www.youtube-nocookie.com/embed/sNwV5GaGODY?controls=1&rel=0&disablekb=1&autoplay=1&loop=0&mute=0&modestbranding=1`}
+          title="How to use PURAXEL"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </Modal>
+    );
+  };
+
   return (
     <article className={_.container}>
       <section className={_.title__container}>
@@ -16,16 +64,14 @@ export default function Page4() {
         </article>
       </section>
 
-      <section className={_.movie__container}>
-        <iframe
-          width="100%"
-          height="100%"
-          src={`https://www.youtube-nocookie.com/embed/sNwV5GaGODY?controls=1&rel=0&disablekb=1&autoplay=0&loop=0&mute=0&modestbranding=1`}
-          title="How to use PURAXEL"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+      <section
+        className={_.movie__container}
+        onClick={
+          youtubeModalOpen ? handleYoutubeModalClose : handleYoutubeModalOpen
+        }
+      >
+        <YoutubeThumnail />
+        <YoutubeModal />
       </section>
     </article>
   );
