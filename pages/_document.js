ef97c890d0b1988310b368927fc8308ddcc2b18b
oh taejun <rewrite0w0@gmail.com>
2022-09-28 16:00:56 +0900

    mod: 폰트 적용방법 변경

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,18 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css&display=swap"
         /> */}
 
-        <link
+        {/* <link
           rel="stylesheet"
           as="style"
           crossOrigin
           href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.5/static/pretendard.css&display=swap"
+        /> */}
+
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin
+          href="https://unpkg.com/pretendard@1.3.5/dist/web/static/pretendard-dynamic-subset.css&display=swap"
         />
       </Head>
       <body>
