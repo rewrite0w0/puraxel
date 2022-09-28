7f34ec58b06973b457413b5dbe4a65ba01b968c0
oh taejun <rewrite0w0@gmail.com>
2022-09-28 16:03:37 +0900

    mod: roll back

--- pages/_document.js
+++ pages/_document.js
@@ -4,25 +4,11 @@ export default function Document() {
   return (
     <Html lang="kr">
       <Head>
-        {/* <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css&display=swap"
-        /> */}
-
-        {/* <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin
-          href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.5/static/pretendard.css&display=swap"
-        /> */}
-
         <link
           rel="stylesheet"
           as="style"
           crossOrigin
-          href="https://unpkg.com/pretendard@1.3.5/dist/web/static/pretendard-dynamic-subset.css&display=swap"
+          href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.5/static/pretendard.css&display=swap"
         />
       </Head>
       <body>
