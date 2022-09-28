3bbb440af0ebebc2c0ae0fbb25759734b63f53c6
oh taejun <rewrite0w0@gmail.com>
2022-09-28 15:58:30 +0900

    add: document 설정(폰트최적화)

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="kr">
+      <Head>
+        {/* <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css&display=swap"
+        /> */}
+
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin
+          href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.5/static/pretendard.css&display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
